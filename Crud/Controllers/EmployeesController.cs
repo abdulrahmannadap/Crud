29429081cf9c15e9ef6fe081aed3ad425cc5e7f9
Abdul Rahman <63226271+abdulrahmannadap@users.noreply.github.com>
2025-04-29 14:36:11 +0530

    EmployeeController Clean

--- Crud/Controllers/EmployeesController.cs
+++ Crud/Controllers/EmployeesController.cs
@@ -1,6 +1,4 @@
 ﻿using AutoMapper;
-using Crud.Dtos;
-using Crud.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Crud.Controllers
@@ -14,23 +12,6 @@ namespace Crud.Controllers
         {
             _mapper = mapper;
         }
-        // POST: api/employee
-        [HttpPost]
-        public IActionResult CreateEmployee([FromBody] EmployeeCreateDto employeeCreateDto)
-        {
-            if (employeeCreateDto == null)
-            {
-                return BadRequest("Employee data is required.");
-            }
 
-            // Map DTO to Employee model
-            var employee = _mapper.Map<Employee>(employeeCreateDto);
-
-            // You can now save the employee data to the database
-            // Example: Save to a database (code omitted for simplicity)
-
-            // Returning a success response with the employee data
-            return Ok(new { message = "Employee created successfully", employee });
-        }
     }
 }
